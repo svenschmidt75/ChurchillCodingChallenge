fd5a6dae709648936a191fa23213d3dc23e30307
Sven Schmidt <s.schmidt@questintegrity.com>
2017-08-26 08:35:04 -0600

    avoid exceptions from leaving the C API layer

--- CodingChallenge.UnitTest/KDTreeTest.cpp
+++ CodingChallenge.UnitTest/KDTreeTest.cpp
@@ -1,17 +1,27 @@
+/*
+ * Name  : KDTreeTest
+ * Path  :
+ * Use   : Unit tests around KDTree class and behaviour
+ * Author: Sven Schmidt
+ * Date  : 08/26/2017
+ */
 #include "pch.h"
 
+#include "../KDTree/ChurchillHeader.h"
+#include "../KDTree/KDTreeNode.h"
 #include "../KDTree/KDTree.h"
 
 
 using namespace KDTree_NS;
 
 
-TEST(KDTree, Test1) {
+TEST(KDTree, CreateSimple) {
  
     // Arrange
-//    KDTree kdtree;
+    std::vector<Point> points{ Point{0, 1, 1.0f, 1.0f}};
     
     // Act
+    KDTree kdtree(&points[0], &points[0] + 1);
 
     // Assert
 }
--- KDTree/KDTree.cpp
+++ KDTree/KDTree.cpp
@@ -20,22 +20,35 @@ struct SearchContext {
 };
 
 SearchContext * create(Point const * points_begin, Point const  * points_end) {
-    auto search_context = new SearchContext;
-    search_context->kd_tree = std::make_unique<KDTree>(points_begin, points_end);
-    return search_context;
-}
-
-int32_t search(SearchContext * /*sc*/, Rect const /*rect*/, int32_t const /*count*/, Point * /*out_points*/) {
-    return 0;
-}
-
-SearchContext * destroy(SearchContext * sc) {
-    sc->kd_tree = nullptr;
+    try {
+        auto search_context = new SearchContext;
+        search_context->kd_tree = std::make_unique<KDTree>(points_begin, points_end);
+        return search_context;
+    }
+    catch(...) {}
     return nullptr;
 }
 
-
-
-
-KDTree::KDTree(Point const * /*points_begin*/, Point const  * /*points_end*/) {
+int32_t search(SearchContext * /*sc*/, Rect const /*rect*/, int32_t const /*count*/, Point * /*out_points*/) {
+    try {
+        return 0;
+    }
+    catch(...) {}
+    return -1;
 }
+
+SearchContext * destroy(SearchContext * sc) {
+    try {
+        sc->kd_tree = nullptr;
+        return nullptr;
+    }
+    catch (...) {}
+    return sc;
+}
+
+
+
+
+KDTree::KDTree(Point const * points_begin, Point const  * points_end)
+    :
+    root_{std::make_unique<KDTreeNode>(std::vector<Point>(points_begin, points_end), 0)} {}
